35a1a5a9ad33155b761ee465628b5b995cdcd8a7
Alex Johnstone <alexander.h.johnstone@gmail.com>
2014-05-17 19:40:24 -0600

    Landscape awesomeness

--- _Scripts/Environment/Map.cs
+++ _Scripts/Environment/Map.cs
@@ -19,6 +19,10 @@ public class Map : MonoBehaviour {
 	public TerrainData landscapeTerrain;
 
 	private GameObject landscapeHolder;
+
+	public GameObject tree;
+
+	public float terrainHeight;
 	
 	void Start () 
 	{
@@ -29,19 +33,12 @@ public class Map : MonoBehaviour {
 			Debug.Log("Loaded "+levelSelection.ToString());
 			Debug.Log("Map Size is "+mapData.Length +" x "+mapData[0].Length);
 
-			Draw_Map(mapData);
+
+			Extract_Level_Data (mapData);
+
+			Create_Landscape(mapData);
 		}
-
-
-
-	}
-	
-	void Update () 
-	{
-	
-	}
-
-	 
+	}	 
 
 	public Color[][] Read_Map_Image_File (string levelName)
 	{
@@ -79,31 +76,88 @@ public class Map : MonoBehaviour {
 		}
 	}
 
-	private void Draw_Map (Color[][] mapData)
+	private void Extract_Level_Data (Color[][] mapData)
 	{
-		//create new terrain
-		//GameObject mapTerrain = new Terrain();
 
-		//mapTerrain.terrainData = landscapeTerrain;
-		//mapTerrain.name = "Terrain";
+	}
 
 
+	private void Create_Landscape (Color[][] mapData)
+	{
+		int terrainScalar = (int)512/mapData.Length;
+
+		float[,] heights = new float[mapData.Length*terrainScalar,mapData[0].Length*terrainScalar];
 
 		landscapeHolder = new GameObject();
 		landscapeHolder.name = "Landscape Holder";
-
-
-
 
 		for ( int x = 0; x < mapData.Length; x++)
 		{
 			for ( int y = 0; y < mapData[0].Length; y++)
 			{
+				Color pixelColor = mapData [x][y];
+
+				bool tileOpen = true;
+
+				if ( pixelColor.r < 0.05f && pixelColor.g > 0.95f && pixelColor.b < 0.05f )
+				{
+					GameObject treeObject = (GameObject)Instantiate(tree);
+					
+					treeObject.transform.position = new Vector3 (x + 0.5f,mapData[y][x].grayscale * terrainHeight,y - 0.5f);
+					
+					//smooth map
+					mapData[x][y] = (mapData[x+1][y] + mapData[x-1][y] + mapData[x][y+1] + mapData[x][y-1])/4;
+
+					tileOpen = false;
+				}
+
 				GameObject mapTile = (GameObject)Instantiate(landscapeTile);
-				landscapeTile.transform.position = new Vector3 (x,mapData[x][y].grayscale * 3,y);
+				landscapeTile.transform.position = new Vector3 (x + 0.5f,(mapData[y][x].grayscale * terrainHeight)+0.01f,y + 0.5f);
 
 				mapTile.transform.parent = landscapeHolder.transform;
+
+				mapTile.AddComponent<Tile>();
+				mapTile.GetComponent<Tile>().Constructor(new Vector3 (x,mapData[y][x].grayscale * terrainHeight,y),tileOpen,landscapeType.plane);
 			}
 		}
+
+
+
+		for (int h1 = 0 ; h1 < mapData.Length*terrainScalar; h1++)
+		{
+			for (int h2 = 0 ; h2 < mapData[0].Length*terrainScalar; h2++)
+			{
+				int xPos = (int)h1/terrainScalar;
+				int yPos = (int)h2/terrainScalar;
+
+				float xOffset = (h1 - (xPos*terrainScalar));
+				float yOffset = (h2 - (yPos*terrainScalar));
+
+				xOffset = xOffset/terrainScalar;
+				yOffset = yOffset/terrainScalar;
+
+				float value = mapData[xPos][yPos].grayscale;
+
+				if (xPos > 0 && xPos < mapData.Length-1 && yPos > 0 && yPos < mapData[0].Length-1)
+				{
+					float value1 = (((mapData[xPos+1][yPos].grayscale - mapData[xPos-1][yPos].grayscale)*xOffset)+mapData[xPos][yPos].grayscale);
+					float value2 = (((mapData[xPos][yPos+1].grayscale - mapData[xPos][yPos-1].grayscale)*yOffset)+mapData[xPos][yPos].grayscale);
+
+
+					value = (value1 + value2)/2;
+
+					
+					value = (((heights[h1+1,h2] + heights[h1,h2+1] + heights[h1-1,h2] + heights[h1,h2-1])/4)+value)/2;
+				}
+				
+				
+				heights[h1,h2] = value * (terrainHeight/10);
+			}
+			
+		}
+
+		landscapeTerrain.SetHeights(1,1,heights);
+				
+		GameObject mapTerrain = Terrain.CreateTerrainGameObject(landscapeTerrain);
 	}
 }
--- _Scripts/Environment/Tile.cs
+++ _Scripts/Environment/Tile.cs
@@ -8,28 +8,47 @@ public class Tile : MonoBehaviour {
 	private bool highlighted = false;
 	private landscapeType type = landscapeType.plane;
 
-	private Color neutral_Color = new Color (0.8f,0.8f, 0.8f, 0.2f);
+	private Color neutral_Color = new Color (0.8f,0.8f, 0.8f, 0f);
 	private Color highlighted_Color = new Color (0.8f,0, 0, 0.6f);
 	 
 
-	public void Instantiate (Vector3 loc, bool access, landscapeType tileType)
+	public void Constructor (Vector3 loc, bool access, landscapeType tileType)
 	{
 		location = loc;
 		open = access;
 		type = tileType;
+		Set_Highlighted(false);
 
+		Set_Visibility () ;
 	}
 
 	public Vector3 Get_Pos () { return location; }
 
 	public bool Get_Open () { return open; }
 
+	public void Set_Open (bool value) 
+	{ 
+		open = value; 
+		Set_Visibility ();
+	}
+
 	public landscapeType Get_Type () { return type; }
 
 	public void Set_Highlighted (bool value) 
 	{ 
 		highlighted = value; 
-		if (value){	this.renderer.material.color = neutral_Color; }
-		else { this.renderer.material.color = highlighted_Color; }
+		if (open)
+		{
+			if (value){	this.renderer.material.color = highlighted_Color; }
+			else { this.renderer.material.color = neutral_Color; }
+		}
 	}
+
+	public int Get_X () { return (int)location.x; }
+
+	public int Get_Y () { return (int)location.z; }
+
+	public float Get_Height () { return location.y; }
+
+	private void Set_Visibility () { this.gameObject.renderer.enabled = open; }
 }
